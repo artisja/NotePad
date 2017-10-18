04e3c2254830ed698c2b36c70210197fda7bfe37
artisja <artisja@vcu.edu>
2017-10-18 19:20:07 -0400

    Initial Commit

---
+++ NotePad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotePad
+//
+//  Created by Justin Artis on 10/18/17.
+//  Copyright © 2017 Justin Artis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
